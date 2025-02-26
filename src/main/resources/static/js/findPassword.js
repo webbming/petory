38862ac04a1826f843761f69f76e95175821100f
kimjinhoo <wlsgnwkd22@naver.com>
2025-02-27 01:12:24 +0900

    비밀번호 찾기 메시지 수정

--- src/main/resources/static/js/findPassword.js
+++ src/main/resources/static/js/findPassword.js
@@ -4,7 +4,6 @@ const findPasswordForm = document.querySelector("#findPasswordForm")
 findPasswordForm.addEventListener("submit" , async (e) =>{
     const message = document.querySelector(".error")
     e.preventDefault();
-    console.log("폼클릭릭")
     const userId = document.querySelector("#userId").value.trim();
     const email = document.querySelector('#email').value.trim();
 
@@ -26,11 +25,11 @@ findPasswordForm.addEventListener("submit" , async (e) =>{
         if(response.ok){
             const data = await response.json();
             message.innerHTML = "";
-            message.textContent = data.message;
+            message.innerHTML = data.message;
         }else{
             const data = await response.json();
             message.innerHTML = "";
-            message.textContent = data.message;
+            message.innerHTML = data.message;
         }
     }catch (e){
         console.error("요청 실패 " + e)
