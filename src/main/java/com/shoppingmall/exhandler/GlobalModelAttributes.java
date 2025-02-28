44ffd6c3a95a22619246233247477db6ece288f4
kimjinhoo <wlsgnwkd22@naver.com>
2025-03-01 01:51:32 +0900

    타임리프 fragment 적용 및 마이페이지 전역 user 정보 구현중

---
+++ src/main/java/com/shoppingmall/exhandler/GlobalModelAttributes.java
@@ -0,0 +1,33 @@
+package com.shoppingmall.exhandler;
+
+import com.shoppingmall.cart.service.CartService;
+import com.shoppingmall.user.model.User;
+import com.shoppingmall.user.repository.UserRepository;
+import com.shoppingmall.user.service.UserService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
+
+@ControllerAdvice
+public class GlobalModelAttributes {
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final CartService cartService;
+
+    public GlobalModelAttributes(UserService userService, CartService cartService, UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+        this.cartService = cartService;
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model , Principal principal) {
+        if(principal != null) {
+            User user = userRepository.findByUserId(principal.getName());
+            model.addAttribute("user", user);
+            model.addAttribute("cartCount" , user.getCart().getTotalQuantity());
+        }
+    }
+}
